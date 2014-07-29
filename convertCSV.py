720e96cadc7a52f177e3dcef58b65c10f80cf20d
Kamile Mat <kamile.matu@yahoo.com>
2014-07-29 12:30:04 +0100

    removed regions

--- convertCSV.py
+++ convertCSV.py
@@ -13,78 +13,55 @@ def convert(f):
     result = {}
     
     Continents = []
-    Regions = []
     Countries = []
     
     country = {}
-    regionObj = {}
     continentsObj ={}
-    
     continent = ""
-    region = ""
+
     
     lineLength = len(lines)
-    i=1
     
-    while i < lineLength:
+    
+    for line in lines[1:]: #because the first line contains headers
         
-        currentLine = lines[i].split(",")
+        currentLine = line.split(",")
         
         if (currentLine[0] == "Africa" or currentLine[0] == "Asia" or currentLine[0] == "Europe" or currentLine[0] == "Latin America and Caribbean" or currentLine[0] == "Northern America" or currentLine[0] == "Oceania"):
-             if (Regions != {}): #Continents holds Regions which holds countries
+             if (Countries != {}): #Continents holds Regions which holds countries
                  if continent =="":
                      continent = currentLine[0]
                      
                  continentsObj["name"] = continent
-                 continentsObj["children"] = Regions
+                 continentsObj["children"] = Countries
                  Continents.append(continentsObj)
-                 #print(Continents)
+
                  
                  continentsObj = {}
                  
             
             
-             continent = currentLine[0]  
-        
-            
-        elif (currentLine[0] == "Eastern Africa" or currentLine[0] ==  "Central Africa" or currentLine[0] == "Northern Africa" or currentLine[0] == "Southern Africa" or currentLine[0] == "Western Africa" or currentLine[0] == "Eastern Asia" or currentLine[0] == "South-Central Asia" or currentLine[0] == "South-Eastern Asia" or currentLine[0] == "Western Asia" or currentLine[0] == "Eastern Europe" or currentLine[0] == "Northern Europe" or currentLine[0] == "Southern Europe" or currentLine[0] == "Western Europe" or currentLine[0] == "Caribbean" or currentLine[0] == "Central America" or currentLine[0] == "South America" or currentLine[0] == "Australia/New Zealand" or currentLine[0] == "Melanesia" or currentLine[0] == "Micronesia" or currentLine[0] == "Polynesia"):
-            if (Countries != {}): 
-
-
-                 print(currentLine[0])
-
-                 if region =="":
-                     region = currentLine[0]
-                 
-                 regionObj["name"] = region
-                 regionObj["children"] = Countries
-                 Regions.append(regionObj)
-                
-                 regionObj = {}
-                 
-            
-            
-            region = currentLine[0]
+             continent = currentLine[0]
         
         else:
             country["name"] = currentLine[0]
             country["size"] = int(currentLine[1])
-
-            
-            
             Countries.append(country)
+
+            if currentLine[0] == "Wallis and Futuna Islands":
+
+                continentsObj["name"] = continent
+                continentsObj["children"] = Countries
+                Continents.append(continentsObj)
             
             country = {}
-
-
-        i+=1
         
         
     
     
     result["name"] = "flare"
     result["children"] = Continents
-    print(result)
+    #print(result)
     
     return json.dumps(result)
     
@@ -98,7 +75,7 @@ f.close()
 
 data = convert(contents)
 
-f = open("flare.json","w")
+f = open("flare2.json","w")
 f.write(data)
 f.close()
 
